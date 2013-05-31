8985bcaa93986c3b81dc519dfe791f4a6763804a
tranguyenthanh <trant@haloasia.net>
2013-05-31 13:34:44 +0700

    This is version 0.0.1

---
+++ Othello/Othello/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Othello
+//
+//  Created by NTT on 5/31/13.
+//  Copyright (c) 2013 Halo Asia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Othello/Othello/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Othello
+//
+//  Created by NTT on 5/31/13.
+//  Copyright (c) 2013 Halo Asia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
